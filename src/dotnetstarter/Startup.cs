a4ba79834967867f4ccce7b79ddd691e50dfbf9c
atilatosta <atila.tosta@gmail.com>
2017-02-11 15:11:11 -0200

    Fix MVC problem

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -40,14 +40,13 @@ public class Startup
 
     public static void Main(string[] args)
     {
-        var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .Build();
-
         var host = new WebHostBuilder()
-            .UseKestrel()
-            .UseConfiguration(config)
-            .UseStartup<Startup>()
-            .Build();
+                 .UseKestrel()
+                 .UseContentRoot(Directory.GetCurrentDirectory())
+                 .UseIISIntegration()
+                 .UseStartup<Startup>()
+                 .Build();
+
+        host.Run();
     }
 }
