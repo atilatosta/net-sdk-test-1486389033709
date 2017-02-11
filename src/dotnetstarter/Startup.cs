7f26eff575bfd7da2c83e94ebaab1c6493d8d048
atilatosta <atila.tosta@gmail.com>
2017-02-11 13:41:17 -0200

    Added template and MVC configuration

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,13 +1,55 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 public class Startup
 {
-	public void Configure(IApplicationBuilder app)
-	{
-		app.UseDefaultFiles();
-		app.UseStaticFiles();
+    public IConfigurationRoot Configuration { get; }
+
+    public Startup(IHostingEnvironment env)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+
+        Configuration = builder.Build();
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+        loggerFactory.AddDebug();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+        });
     }
 
     public static void Main(string[] args)
@@ -18,6 +60,7 @@ public class Startup
 
         var host = new WebHostBuilder()
                     .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
                     .UseConfiguration(config)
                     .UseStartup<Startup>()
                     .Build();
