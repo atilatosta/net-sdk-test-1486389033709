7cd3255d6dd87c8c02910fb6c4f0ee22ddabee95
atilatosta <atila.tosta@gmail.com>
2017-02-11 15:22:17 -0200

    Fix problem MVC

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
-	public void Configure(IApplicationBuilder app)
-	{
-		app.UseDefaultFiles();
-		app.UseStaticFiles();
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddMvc();
     }
 
     public static void Main(string[] args)
